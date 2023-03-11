b413ba75d067a65ca909fe60990625300fc9776b
HoneyGrey <89571554+xi-fengw@users.noreply.github.com>
2023-03-12 00:55:26 +0800

    Update segmenttree.cpp

--- segmenttree.cpp
+++ segmenttree.cpp
@@ -1,116 +1,122 @@
-template<class Info, class Tag>
-struct SegTree {
-    int n;
-    vector<Info> tr;
-    vector<Tag> tag;
-    SegTree() = default;
-    SegTree(int n) : n(n), tr(n + 5 << 2), tag(n + 5 << 2) {}
-    SegTree(int n, const vector<Info> a) : SegTree(n) {
-        function<void(int, int, int)> build = [&](int u, int l, int r) {
-            if (l == r) {
-                tr[u] = a[r];
-                return;
-            }
-            int mid = l + r >> 1;
-            build(u << 1, l, mid);
-            build(u << 1 | 1, mid + 1, r);
-            pushup(u);
-        };
-        build(1, 1, n);
-    }
-    void pushup(int u) {
-        tr[u] = tr[u << 1] + tr[u << 1 | 1];
-    }
-    void apply(int u, const Tag &v) {
-        tr[u].apply(v);
-        tag[u].merge(v);
-    }
-    void pushdown(int u) {
-        apply(u << 1, tag[u]);
-        apply(u << 1 | 1, tag[u]);
-        tag[u] = Tag();
-    }
-    void modify(int u, int l, int r, int x, const Info &v) {
-        if (l > x || r < x) {
-            return;
-        }
-        if (l == r) {
-            tr[u] = v;
-            return;
-        }
-        pushdown(u);
-        int mid = l + r >> 1;
-        modify(u << 1, l, mid, x, v);
-        modify(u << 1 | 1, mid + 1, r, x, v);
-        pushup(u);
-    }
-    void rangeModify(int u, int l, int r, int ql, int qr, const Tag &v) {
-        if (l > qr || r < ql) {
-            return;
-        }
-        if (ql <= l && r <= qr) {
-            return apply(u, v);
-        }
-        pushdown(u);
-        int mid = l + r >> 1;
-        rangeModify(u << 1, l, mid, ql, qr, v);
-        rangeModify(u << 1 | 1, mid + 1, r, ql, qr, v);
-        pushup(u);
-    }
-    Info query(int u, int l, int r, int ql, int qr) {
-        if (l > qr || r < ql) {
-            return Info();
-        }
-        if (ql <= l && r <= qr) {
-            return tr[u];
-        }
-        pushdown(u);
-        int mid = l + r >> 1;
-        return query(u << 1, l, mid, ql, qr) + query(u << 1 | 1, mid + 1, r, ql, qr);
-    }
-    template<class F>
-    int find(int u, int l, int r, int ql, int qr, const F &f) {
-        if (l > qr || r < ql || f(tr[u])) {
-            return 0;
-        }
-        if (l == r) {
-            return r;
-        }
-        pushdown(u);
-        int mid = l + r >> 1;
-        int res = find(u << 1, l, mid, ql, qr, f);
-        return res ? res : find(u << 1 | 1, mid + 1, r, ql, qr, f);
-    }
- 
-    Info query(int l, int r) {
-        return query(1, 1, n, l, r);
-    }
-    void modify(int x, const Info &v) {
-        return modify(1, 1, n, x, v);
-    }
-    void rangeModify(int l, int r, const Tag &o) {
-        return rangeModify(1, 1, n, l, r, o);
-    }
-    template<class F>
-    int find(int l, int r, const F &f) {
-        return find(1, 1, n, l, r, f);
-    }
-};
- 
-struct Tag {
-    int add = 0;
-    void merge(const Tag &o) {
-        add += o.add;
-    }
-};
- 
-struct Info {
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <vector>
 
-    void apply(const Tag &o) {
-    }
+namespace Lazysegmenttree {
+const static int N = 200010;
+template <class T> constexpr T inf = std::numeric_limits<T>::max() / 2;
+
+// @ range add get range min
+struct Info {
+  int minv;
+
+  Info() : minv(inf<int>) {}
+  Info(int _minv) : minv(_minv) {}
 };
- 
-Info operator+(const Info &l, const Info &r) {
-    return {
-    };
+
+Info operator+(const Info &a, const Info &b) {
+  Info c;
+  c.minv = std::min(a.minv, b.minv);
+  return c;
 }
+
+struct Tag {
+  int add;
+
+  Tag() : add(0) {}
+  Tag(int _add) : add(_add) {}
+};
+
+void apply(Info &a, Tag &b, const Tag &c) {
+  a.minv = a.minv + c.add;
+  b.add = b.add + c.add;
+}
+
+Info info[N << 2];
+Tag tag[N << 2];
+
+void pull(int p) { info[p] = info[p * 2] + info[p * 2 + 1]; }
+
+void build(int p, int l, int r, std::vector<int> &init) {
+  if (l == r) {
+    info[p].minv = init[l];
+    return;
+  }
+
+  int mid = (l + r) >> 1;
+  build(p * 2, l, mid, init), build(p * 2 + 1, mid + 1, r, init);
+  pull(p);
+}
+
+void apply(int p, const Tag &v) { apply(info[p], tag[p], v); }
+
+void push(int p) {
+  apply(p * 2, tag[p]);
+  apply(p * 2 + 1, tag[p]);
+  tag[p] = Tag();
+}
+
+void set(int p, int l, int r, int x, const Info &v) {
+  if (l == r) {
+    info[p] = v;
+    return;
+  }
+  int mid = (l + r) >> 1;
+  push(p);
+}
+
+void modify(int p, int l, int r, int ln, int rn, const Tag &v) {
+  if (l > rn || r < ln)
+    return;
+  if (l >= ln && r <= rn) {
+    apply(p, v);
+    return;
+  }
+
+  push(p);
+  int mid = (l + r) >> 1;
+  modify(p * 2, l, mid, ln, rn, v);
+  modify(p * 2 + 1, mid + 1, r, ln, rn, v);
+  pull(p);
+}
+
+Info query(int p, int l, int r, int ln, int rn) {
+  if (l > rn || r < ln)
+    return Info();
+  if (l >= ln && r <= rn) {
+    return info[p];
+  }
+
+  push(p);
+  int mid = (l + r) >> 1;
+  return query(p * 2, l, mid, ln, rn) + query(p * 2 + 1, mid + 1, r, ln, rn);
+}
+
+// @ return first index >= v
+int lower(int p, int l, int r, int ln, int rn, const Info &v) {
+  push(p);
+  if (l >= ln && r <= rn) {
+    if (info[p].minv < v.minv)
+      return -1;
+    if (l == r)
+      return l;
+    int mid = (l + r) / 2;
+    if (info[p * 2].minv >= v.minv)
+      return lower(p * 2, l, mid, ln, rn, v);
+    return lower(p * 2 + 1, mid + 1, r, ln, rn, v);
+  }
+  int mid = (l + r) / 2;
+
+  if (rn <= mid)
+    return lower(p * 2, l, mid, ln, rn, v);
+  else if (mid < l)
+    return lower(p * 2 + 1, mid + 1, r, ln, rn, v);
+  else {
+    int pos = lower(p * 2, l, mid, ln, mid, v);
+    if (!~pos)
+      return lower(p * 2 + 1, mid + 1, r, mid + 1, rn, v);
+    return pos;
+  }
+}
+} // namespace Lazysegmenttree
